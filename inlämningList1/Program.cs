4087937b18e1107ef930287a80ff510d56ceb304
Knugen <isel06002@utb.vaxjo.se>
2024-02-02 12:40:24 +0100

    Add project files.

---
+++ inlämningList1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+
+namespace inlämingList1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> namn = new List<string>();
+            string svar = "";
+            do
+            {
+                Console.WriteLine("Skriv in ett namn");
+                svar = Console.ReadLine();
+                namn.Add(svar);
+            } while (svar != "");
+            namn.Sort();
+            namn.Reverse();
+            foreach(string n in namn)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
